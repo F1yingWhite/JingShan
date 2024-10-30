16749d59503655bb0e228d767cd085b642bad665
F1yingWhite <81723334@qq.com>
2024-10-30 19:44:38 +0800

    feat:缘起以及初始页面

--- front-end_new/src/app/layout.tsx
+++ front-end_new/src/app/layout.tsx
@@ -3,6 +3,12 @@ import "./globals.css";
 import { AntdRegistry } from '@ant-design/nextjs-registry';
 import NavBar from "@/components/NavBar";
 import { Content } from "antd/es/layout/layout";
+import { Noto_Sans_SC } from "next/font/google"
+
+const noto_sans_sc = Noto_Sans_SC({
+  display: 'swap',
+  subsets: ['vietnamese'],
+})
 
 export default function RootLayout({
   children,
@@ -10,21 +16,21 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body>
+    <html lang="cn">
+      <body className={noto_sans_sc.className}>
         <AntdRegistry>
           <ConfigProvider
             theme={{
               token: {
-                colorPrimary: '#A1845A',
+                colorPrimary: '#8d7651',
                 borderRadius: 5,
-                colorText: '#FFFFFF',
-                colorBgContainer: '#A1845A',
               },
             }}
           >
             <Layout className="h-screen">
-              <NavBar />
+              <div className="fixed w-full z-10">
+                <NavBar />
+              </div>
               <Content >
                 {children}
               </Content>
--- front-end_new/src/app/page.tsx
+++ front-end_new/src/app/page.tsx
@@ -1,10 +1,66 @@
-import { Button } from 'antd'
-import React from 'react'
+'use client'
+import React, { useEffect, useState } from 'react';
+import { Layout, Input } from 'antd';
+import { Footer, Content } from 'antd/es/layout/layout';
+import Image from 'next/image';
+
+const { Search } = Input;
 
 export default function Page() {
+  const bgStyle = {
+    backgroundColor: '#f4f1ea',
+    backgroundImage: 'url(/mainPage.png)',
+    backgroundSize: 'cover',
+    backgroundPosition: 'center',
+  };
+
+  const footStyle = {
+    backgroundColor: "#92754B"
+  };
+
+  const search = (value: string) => {
+    console.log(value);
+  };
+
+  const centerCol = "flex flex-col items-center";
+
+  const [showFooter, setShowFooter] = useState(true);
+
+  const checkHeight = () => {
+    setShowFooter(window.innerHeight > 400);
+  };
+
+  useEffect(() => {
+    checkHeight();
+    window.addEventListener('resize', checkHeight);
+    return () => window.removeEventListener('resize', checkHeight);
+  }, []);
+
   return (
-    <div className='h-full'>
-      <Button>main</Button>
-    </div>
-  )
+    <Layout className='h-full' style={bgStyle}>
+      <Content className={centerCol}>
+        <div className="w-1/2 flex flex-col items-center mt-20">
+          <Image src="/天下径山.png" alt='天下径山' width={400} height={100} />
+          <Search
+            allowClear
+            enterButton="搜索"
+            size="large"
+            placeholder='请输入关键字'
+            className='mt-3 text-black'
+            onSearch={search}
+          />
+        </div>
+      </Content>
+      {showFooter && (
+        <Footer style={footStyle} className={centerCol}>
+          <div className="flex justify-center text-white">
+            @2024 powered by eagle-lab
+          </div>
+          <div className="flex justify-center text-white">
+            联系我们: xlz24@163.com
+          </div>
+        </Footer>
+      )}
+    </Layout>
+  );
 }
