a7aff2abf76cf69d7e26cacedd47ddcd6a80c50c
F1yingWhite <81723334@qq.com>
2024-12-19 20:09:08 +0800

    fix:滚动条

--- front-end/src/components/ScrollList.tsx
+++ front-end/src/components/ScrollList.tsx
@@ -1,3 +1,4 @@
+'use client'
 import { Divider, List, Skeleton } from 'antd'
 import Link from 'next/link';
 import React, { useEffect, useState } from 'react'
@@ -21,7 +22,7 @@ export default function ScollList({ loadDataApi, title }: ScollListProps) {
   const loadMoreData = () => {
     if (loading) return
     setLoading(true)
-    loadDataApi(Math.ceil(data.length / 20) + 1, 20).then((res) => {
+    loadDataApi(Math.ceil(data.length / 100) + 1, 100).then((res) => {
       setTotal(res.total_num)
       setData(prevData => [...prevData, ...res.results])
       setLoading(false)
@@ -49,7 +50,6 @@ export default function ScollList({ loadDataApi, title }: ScollListProps) {
           scrollableTarget="scrollableDiv"
         >
           <List
-            style={{ overflow: 'auto' }}
             dataSource={data}
             renderItem={(item) => (
               <List.Item key={item.name}>
